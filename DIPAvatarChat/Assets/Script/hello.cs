a3dba46147c9c4427b1cf643950523d83fe827a9
Li Tianchu <tianchu0522@gmail.com>
2023-09-03 19:13:19 +0800

    firebase dependencies

--- DIPAvatarChat/Assets/Script/hello.cs
+++ DIPAvatarChat/Assets/Script/hello.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase;
+using Firebase.Analytics;
 using UnityEngine;
 
 public class hello : MonoBehaviour
@@ -7,12 +9,16 @@ public class hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Method intentionally left empty.
     }
 }
