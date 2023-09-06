cfc1329c1d778594d2bd58de686e4ee9a67f632c
Aloysius Goh <aloysiusgohkw@gmail.com>
2023-09-06 16:05:39 +0800

    updated Malvin Login and Register page with Firebase Auth

--- DIPAvatarChat/Assets/Script/AuthManagerV2.cs
+++ DIPAvatarChat/Assets/Script/AuthManagerV2.cs
@@ -5,10 +5,10 @@ using Firebase.Auth;
 using TMPro;
 using System.Threading.Tasks;
 
-public class PhoneAuthManager : MonoBehaviour
+public class AuthManagerV2 : MonoBehaviour
 {
     //Firebase variables
-    /*[Header("Firebase")]
+    [Header("Firebase")]
     public DependencyStatus dependencyStatus;
     public FirebaseAuth auth;
     public FirebaseUser User;
@@ -22,7 +22,8 @@ public class PhoneAuthManager : MonoBehaviour
 
     //Register variables
     [Header("Register")]
-    public TMP_InputField phoneNumberRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
     public TMP_Text warningRegisterText;
 
     void Awake()
@@ -60,7 +61,7 @@ public class PhoneAuthManager : MonoBehaviour
     public void RegisterButton()
     {
         //Call the register coroutine passing the email, password, and username
-        StartCoroutine(Register(phoneNumberRegisterField.text));
+        StartCoroutine(Register(emailRegisterField.text, passwordRegisterField.text));
     }
 
     private IEnumerator Login(string _email, string _password)
@@ -97,6 +98,7 @@ public class PhoneAuthManager : MonoBehaviour
                     break;
             }
             warningLoginText.text = message;
+            confirmLoginText.text = "";
         }
         else
         {
@@ -109,16 +111,73 @@ public class PhoneAuthManager : MonoBehaviour
         }
     }
 
-    private IEnumerator Register(string _phoneNumber)
+    private IEnumerator Register(string _email, string _password)
     {
-        if (_phoneNumber == "")
-        {
-            //If the username field is blank show a warning
-            warningRegisterText.text = "Missing Phone Number";
-        }
-        else
-        {
-            
-        }
-    }*/
+
+            //Call the Firebase auth signin function passing the email and password
+            Task<AuthResult> RegisterTask = auth.CreateUserWithEmailAndPasswordAsync(_email, _password);
+            //Wait until the task completes
+            yield return new WaitUntil(predicate: () => RegisterTask.IsCompleted);
+
+            if (RegisterTask.Exception != null)
+            {
+                //If there are errors handle them
+                Debug.LogWarning(message: $"Failed to register task with {RegisterTask.Exception}");
+                FirebaseException firebaseEx = RegisterTask.Exception.GetBaseException() as FirebaseException;
+                AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+                string message = "Register Failed!";
+                switch (errorCode)
+                {
+                    case AuthError.MissingEmail:
+                        message = "Missing Email";
+                        break;
+                    case AuthError.MissingPassword:
+                        message = "Missing Password";
+                        break;
+                    case AuthError.WeakPassword:
+                        message = "Weak Password";
+                        break;
+                    case AuthError.EmailAlreadyInUse:
+                        message = "Email Already In Use";
+                        break;
+                }
+                warningRegisterText.text = message;
+            }
+            else
+            {
+                //User has now been created
+                //Now get the result
+                User = RegisterTask.Result.User;
+
+                if (User != null)
+                {
+                /*//Create a user profile and set the username
+                UserProfile profile = new UserProfile { DisplayName = _username };
+
+                //Call the Firebase auth update user profile function passing the profile with the username
+                Task ProfileTask = User.UpdateUserProfileAsync(profile);
+                //Wait until the task completes
+                yield return new WaitUntil(predicate: () => ProfileTask.IsCompleted);
+
+                if (ProfileTask.Exception != null)
+                {
+                    //If there are errors handle them
+                    Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
+                    FirebaseException firebaseEx = ProfileTask.Exception.GetBaseException() as FirebaseException;
+                    AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+                    warningRegisterText.text = "Username Set Failed!";
+                }
+                else
+                {
+                    //Username is now set
+                    //Now return to login screen
+                    UIManager.instance.LoginScreen();
+                    warningRegisterText.text = "";
+                }*/
+                UIManager.instance.LoginScreen();
+                warningRegisterText.text = "";
+            }
+            }
+    }
 }
