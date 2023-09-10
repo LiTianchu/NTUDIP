b5e544cbbb46145967674dc215e4773985bf5efb
rushdina <redredrushrush@gmail.com>
2023-09-11 01:22:43 +0800

    editted chatUI

--- DIPAvatarChat/Assets/Script/Pages/BackChatList.cs
+++ DIPAvatarChat/Assets/Script/Pages/BackChatList.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewChat : MonoBehaviour
+public class BackToChatList : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
