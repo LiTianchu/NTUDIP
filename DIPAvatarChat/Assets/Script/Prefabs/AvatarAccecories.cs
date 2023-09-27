724e2a22b790ccaf69a70c6dd73bcadd87ef18ae
tayywen <142387894+tayywen@users.noreply.github.com>
2023-09-27 13:21:30 +0800

    script for avatar accessories

--- DIPAvatarChat/Assets/Script/Prefabs/AvatarAccecories.cs
+++ DIPAvatarChat/Assets/Script/Prefabs/AvatarAccecories.cs
@@ -11,20 +11,33 @@ public class AvatarAccessories : MonoBehaviour
     public Transform AccessoryPanel; // Parent transform for the chat messages
     public Button selectAccessoryButton; // Reference to the send button in the Inspector
 
+    private GameObject previousClone;
     
 
     // Start is called before the first frame update
     void Start()
     {
-
         // Attach an event handler to the send button's click event
         selectAccessoryButton.onClick.AddListener(InstantiateAccessory);
     }
 
     public void InstantiateAccessory()
-    {        
-            // Create a new chat message GameObject
-            Instantiate(AccessoryPrefab, AccessoryPanel);
+    {   
+        Debug.Log("InstantiateAccessory called");
+
+        // Destroy the previous clone if it exists
+        if (previousClone != null)
+        {
+            Debug.Log("Destroying previousClone");
+            Destroy(previousClone);
+        }
+
+        // Create a new chat message GameObject
+        GameObject newClone = Instantiate(AccessoryPrefab, AccessoryPanel);
+
+        // Assign the newClone to the previousClone variable
+        previousClone = newClone;
+
     }
 
 
