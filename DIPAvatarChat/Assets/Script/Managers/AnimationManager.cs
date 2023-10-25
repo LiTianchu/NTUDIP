8a74d51dc704e8f3d5fb8b341242490e775b0b83
Aloysius Goh <aloysiusgohkw@gmail.com>
2023-10-25 13:40:19 +0800

    try animator

---
+++ DIPAvatarChat/Assets/Script/Managers/AnimationManager.cs
@@ -0,0 +1,24 @@
+using Firebase.Firestore;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AnimationManager : Singleton<AnimationManager>
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayAnimation()
+    {
+        
+    }
+}
