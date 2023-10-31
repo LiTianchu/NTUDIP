91b773430dd3de84ab969b69112bf2d95f37002a
Aloysius Goh <aloysiusgohkw@gmail.com>
2023-10-31 18:53:48 +0800

    fix chat bubble length

--- DIPAvatarChat/Assets/Script/UI/AutoResizeChatBubble.cs
+++ DIPAvatarChat/Assets/Script/UI/AutoResizeChatBubble.cs
@@ -9,11 +9,43 @@ public class AutoResizeChatBubble : MonoBehaviour
     public RectTransform chatBubbleBox;    // Reference to your chat bubble RectTransform
     public RectTransform chatBubbleImage;
     public TextMeshProUGUI message; // Reference to your TMP Text component
+    float minHeight = 20f;
+    float minWidth = 40f;
+    float maxWidth = 220f;
+    float paddingHeight = 2f;
+    float paddingWidth = 2f;
+    float textHeight;
+    float textWidth;
 
     private void Start()
     {
-        float textHeight = message.preferredHeight;
-        chatBubbleBox.sizeDelta = new Vector2(chatBubbleBox.sizeDelta.x, textHeight);
-        chatBubbleImage.sizeDelta = new Vector2(chatBubbleImage.sizeDelta.x, textHeight);
+        AutoResizeHeight();
+        AutoResizeWidth();
+
+        chatBubbleBox.sizeDelta = new Vector2(textWidth, textHeight + paddingHeight);
+        chatBubbleImage.sizeDelta = new Vector2(textWidth, textHeight + paddingHeight);
+    }
+
+    private void AutoResizeHeight()
+    {
+        textHeight = minHeight;
+        if (message.preferredHeight > minHeight)
+        {
+            textHeight = message.preferredHeight;
+        }
+    }
+
+    private void AutoResizeWidth()
+    {
+        textWidth = minWidth;
+        if (message.preferredWidth > minWidth)
+        {
+            textWidth = message.preferredWidth;
+        }
+
+        if (message.preferredWidth > maxWidth)
+        {
+            textWidth = maxWidth;
+        }
     }
 }
