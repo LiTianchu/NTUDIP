bd5beb2db3014666ea40bb21fd5d05b3fa8a7afd
Aloysius Goh <aloysiusgohkw@gmail.com>
2023-11-15 01:26:50 +0800

    t

--- DIPAvatarChat/Assets/Script/Managers/AvatarManager.cs
+++ DIPAvatarChat/Assets/Script/Managers/AvatarManager.cs
@@ -36,7 +36,7 @@ public class AvatarManager : Singleton<AvatarManager>
     public readonly Vector3 SHOES_SCALE = new Vector3(0.011f, 0.011f, 0.011f);
     public readonly Quaternion SHOES_ROTATION = Quaternion.Euler(180f, 90f, 90f);
 
-    //pos for ears
+    /*/pos for ears
     public readonly Vector3 EARS_POS = new Vector3(0f, 0f, 0f);
     public readonly Vector3 EARS_SCALE = new Vector3(0.01f, 0.01f, 0.01f);
     public readonly Quaternion EARS_ROTATION = Quaternion.Euler(0f, 180f, 0f);
@@ -44,7 +44,7 @@ public class AvatarManager : Singleton<AvatarManager>
     //pos for tail
     public readonly Vector3 TAIL_POS = new Vector3(0.000594871f, 0.0002483991f, -0.004111664f);
     public readonly Vector3 TAIL_SCALE = new Vector3(0.01f, 0.01f, 0.01f);
-    public readonly Quaternion TAIL_ROTATION = Quaternion.Euler(94.58002f, 35.286f, 130.632f);
+    public readonly Quaternion TAIL_ROTATION = Quaternion.Euler(94.58002f, 35.286f, 130.632f);*/
 
     public readonly Vector3 TEXT_BUBBLE_POS = new Vector3(0, 4.25f, 0);
 
